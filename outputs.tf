a47214da24884584931c341b84a2787685cdb0f9
rafael.sathler <rafael.sathler@corp.globo.com>
2019-08-02 20:12:17 -0300

    Ok, 5am, it works.  For real.

--- outputs.tf
+++ outputs.tf
@@ -2,51 +2,23 @@
 # AWS ECS SERVICE
 # ---------------------------------------------------------------------------------------------------------------------
 output "aws_ecs_service_service_id" {
-  description = "The Amazon Resource Name (ARN) that identifies the service." 
+  description = "The Amazon Resource Name (ARN) that identifies the service."
   value       = aws_ecs_service.service.id
 }
 
 output "aws_ecs_service_service_name" {
-  description = "The name of the service." 
+  description = "The name of the service."
   value       = aws_ecs_service.service.name
 }
 
 output "aws_ecs_service_service_cluster" {
-  description = "The Amazon Resource Name (ARN) of cluster which the service runs on." 
+  description = "The Amazon Resource Name (ARN) of cluster which the service runs on."
   value       = aws_ecs_service.service.cluster
 }
 
 output "aws_ecs_service_service_desired_count" {
-  description = "The number of instances of the task definition" 
+  description = "The number of instances of the task definition"
   value       = aws_ecs_service.service.desired_count
-}
-
-# ---------------------------------------------------------------------------------------------------------------------
-# AWS LOAD BALANCER
-# ---------------------------------------------------------------------------------------------------------------------
-output "lb_id" {
-  description = "$${var.name_preffix} Load Balancer ID"
-  value       = aws_lb.lb.id
-}
-
-output "lb_arn" {
-  description = "$${var.name_preffix} Load Balancer ARN"
-  value       = aws_lb.lb.arn
-}
-
-output "lb_arn_suffix" {
-  description = "$${var.name_preffix} Load Balancer ARN Suffix"
-  value       = aws_lb.lb.arn_suffix
-}
-
-output "lb_dns_name" {
-  description = "$${var.name_preffix} Load Balancer DNS Name"
-  value       = aws_lb.lb.dns_name
-}
-
-output "lb_zone_id" {
-  description = "$${var.name_preffix} Load Balancer Zone ID"
-  value       = aws_lb.lb.zone_id
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
