d519a5f06f5086fe4d90842f50406b3aa963368f
rafael.sathler <rafael.sathler@corp.globo.com>
2019-08-02 20:13:03 -0300

    This is why git rebase is a horrible horrible thing.

--- outputs.tf
+++ outputs.tf
@@ -24,25 +24,6 @@ output "aws_ecs_service_service_desired_count" {
 # ---------------------------------------------------------------------------------------------------------------------
 # AWS SECURITY GROUPS
 # ---------------------------------------------------------------------------------------------------------------------
-output "lb_sg_id" {
-  description = "$${var.name_preffix} Load Balancer Security Group - The ID of the security group"
-  value       = aws_security_group.lb_sg.id
-}
-
-output "lb_sg_arn" {
-  description = "$${var.name_preffix} Load Balancer Security Group - The ARN of the security group"
-  value       = aws_security_group.lb_sg.arn
-}
-
-output "lb_sg_name" {
-  description = "$${var.name_preffix} Load Balancer Security Group - The name of the security group"
-  value       = aws_security_group.lb_sg.name
-}
-
-output "lb_sg_description" {
-  description = "$${var.name_preffix} Load Balancer Security Group - The description of the security group"
-  value       = aws_security_group.lb_sg.description
-}
 
 output "ecs_tasks_sg_id" {
   description = "$${var.name_preffix} ECS Tasks Security Group - The ID of the security group"
